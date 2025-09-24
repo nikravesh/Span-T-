0aef9eb321fe97b7df3a3ad7438103c7edba9cb1
Alireza <a.z.nikravesh@gmail.comm>
2025-09-24 13:02:46 +0330

    added benchmark

---
+++ BenchmarkCsv.cs
@@ -0,0 +1,31 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace Span.PracticalSample;
+public class BenchmarkCsv
+{
+    private string[] _lines;
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        _lines = File.ReadAllLines("data.csv");
+    }
+
+    [Benchmark]
+    public void SpanParsing()
+    {
+        foreach (var line in _lines)
+        {
+            var record = CsvParser.ParseWithSpan(line.AsSpan());
+        }
+    }
+
+    [Benchmark]
+    public void StringSplitParsing()
+    {
+        foreach (var line in _lines)
+        {
+            var record = CsvParser.ParseWithString(line);
+        }
+    }
+}
--- CsvParser.cs
+++ CsvParser.cs
@@ -1,7 +1,7 @@
 ﻿namespace Span.PracticalSample;
 public class CsvParser
 {
-    public static (string name,uint age,string job) ParsLine(ReadOnlySpan<char> line)
+    public static (string name,uint age,string job) ParseWithSpan(ReadOnlySpan<char> line)
     {
         var firstComma = line.IndexOf(',');
         var secondComma = line.Slice(firstComma+1).IndexOf(",") + firstComma + 1;
@@ -11,5 +11,11 @@ public class CsvParser
         var job = line.Slice(secondComma + 1);
 
         return (name.ToString(), uint.Parse(age), job.ToString());
+    }
+
+    public static (string Name, int Age, string Job) ParseWithString(string line)
+    {
+        var parts = line.Split(',');
+        return (parts[0], int.Parse(parts[1]), parts[2]);
     }
 }
