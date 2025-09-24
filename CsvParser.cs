760898b7415ebd5c987ede3b0e3c8b575d66b302
Alireza <a.z.nikravesh@gmail.comm>
2025-09-24 12:56:07 +0330

    used csvparser

--- CsvParser.cs
+++ CsvParser.cs
@@ -4,7 +4,7 @@ public class CsvParser
     public static (string name,uint age,string job) ParsLine(ReadOnlySpan<char> line)
     {
         var firstComma = line.IndexOf(',');
-        var secondComma = line.Slice(firstComma+1).IndexOf(",");
+        var secondComma = line.Slice(firstComma+1).IndexOf(",") + firstComma + 1;
 
         var name= line.Slice(0, firstComma);
         var age = line.Slice(firstComma + 1, secondComma - firstComma - 1);
--- Program.cs
+++ Program.cs
@@ -1,1 +1,17 @@
 ﻿
+using Span.PracticalSample;
+
+Console.WriteLine("Reading CSV with Span<T>...");
+Console.WriteLine("------------------------------------------------");
+
+var lines = await File.ReadAllLinesAsync("./data.csv");
+
+foreach (var line in lines)
+{
+    var record =  CsvParser.ParsLine(line);
+
+    Console.WriteLine($"Name: {record.name}, Age:{record.age}, Job: {record.job}");
+    Console.WriteLine("------------------------------------------------");
+}
+
+Console.ReadKey();
